99ddd83937f1c029dd31f0e8be6df55b447ba572
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-17 10:24:54 +0900

    레이어별 필요한 클래스 파일 작성

--- src/main/java/com/project/hrbank/backup/controller/BackupController.java
+++ src/main/java/com/project/hrbank/backup/controller/BackupController.java
@@ -1,41 +1,43 @@
 package com.project.hrbank.backup.controller;
 
-import com.project.hrbank.backup.dto.response.BackupDto;
-import com.project.hrbank.backup.dto.response.CursorPageResponseBackupDto;
-import com.project.hrbank.backup.service.BackupService;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.hrbank.backup.dto.response.BackupDto;
+import com.project.hrbank.backup.dto.response.CursorPageResponseBackupDto;
+import com.project.hrbank.backup.service.BackupService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/backups")
 @RequiredArgsConstructor
 public class BackupController {
 
-    private final BackupService backupService;
+	private final BackupService backupService;
 
-    @GetMapping
-    public ResponseEntity<CursorPageResponseBackupDto> findAll() {
-        CursorPageResponseBackupDto backupDtos = backupService.findAll();
-        return ResponseEntity.ok().body(backupDtos);
-    }
+	@GetMapping
+	public ResponseEntity<CursorPageResponseBackupDto> findAll() {
+		CursorPageResponseBackupDto backupDtos = backupService.findAll();
+		return ResponseEntity.ok().body(backupDtos);
+	}
 
-    @PostMapping
-    public ResponseEntity<BackupDto> backup(HttpServletRequest request) {
-        String clientIpAddr = request.getRemoteAddr();
-        BackupDto backup = backupService.backup();
-        return ResponseEntity.ok().body(backup);
-    }
+	@PostMapping
+	public ResponseEntity<BackupDto> backup(HttpServletRequest request) {
+		String clientIpAddr = request.getRemoteAddr();
+		BackupDto backup = backupService.backup();
+		return ResponseEntity.ok().body(backup);
+	}
 
-    // DASH BOARD 에서 사용
-    @GetMapping("/latest")
-    public ResponseEntity<BackupDto> findLatest() {
+	// DASH BOARD 에서 사용
+	@GetMapping("/latest")
+	public ResponseEntity<BackupDto> findLatest() {
 		BackupDto backupDto = backupService.findLatest();
 		return ResponseEntity.ok().body(backupDto);
-    }
+	}
 
 }
--- src/main/java/com/project/hrbank/backup/domain/Backup.java
+++ src/main/java/com/project/hrbank/backup/domain/Backup.java
@@ -1,5 +1,10 @@
 package com.project.hrbank.backup.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -9,11 +14,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "backups")
@@ -21,34 +23,34 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class Backup {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "backup_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "backup_id")
+	private Long id;
 
-    @Column(name = "worker", nullable = false)
-    private String worker;
+	@Column(name = "worker", nullable = false)
+	private String worker;
 
-    @Column(name = "started_at", nullable = false)
-    private LocalDateTime startedAt;
+	@Column(name = "started_at", nullable = false)
+	private LocalDateTime startedAt;
 
-    @Column(name = "ended_at")
-    private LocalDateTime endedAt;
+	@Column(name = "ended_at")
+	private LocalDateTime endedAt;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
-    private Status status;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status", nullable = false)
+	private Status status;
 
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
 
-    // file_id 연관관계 맺기 1..0 : 1 여기가 주인임!
+	// file_id 연관관계 맺기 1..0 : 1 여기가 주인임!
 
+	public Backup(String worker, Status status) {
+		this.worker = worker;
+		this.status = status;
+		this.startedAt = LocalDateTime.now();
+	}
 
-    public Backup(String worker, Status status) {
-        this.worker = worker;
-        this.status = status;
-        this.startedAt = LocalDateTime.now();
-    }
 }
