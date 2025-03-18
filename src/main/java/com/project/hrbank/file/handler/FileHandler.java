e32d9e561c74dd70206cb3d2578198a7c128711c
kbh <xjvm7001@naver.com>
2025-03-18 13:08:16 +0900

    Feat: 'FileHandler 구현'

--- src/main/java/com/project/hrbank/file/handler/FileHandler.java
+++ src/main/java/com/project/hrbank/file/handler/FileHandler.java
@@ -1,8 +1,12 @@
 package com.project.hrbank.file.handler;
 
 import java.io.IOException;
+import java.nio.file.Path;
+
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.project.hrbank.file.entity.FileEntity;
 
 public interface FileHandler {
 
@@ -21,12 +25,11 @@ public interface FileHandler {
 	void processFile(MultipartFile file) throws IOException;
 
 	/**
-	 *
-	 * @param fileResource 다운로드할 파일 리소스
+	 * @param filePath 파일 경로
 	 * @param fileName 파일명
 	 * @return 파일 다운로드 응답 객체
 	 * @throws IOException 파일 처리 중 오류 발생 가능
 	 */
-	Resource handleDownload(Resource fileResource, String fileName) throws IOException;
+	FileEntity handleDownload(Path filePath, String fileName) throws IOException;
 
 }
--- src/main/java/com/project/hrbank/file/handler/impl/ImageFileHandler.java
+++ src/main/java/com/project/hrbank/file/handler/impl/ImageFileHandler.java
@@ -1,26 +1,37 @@
 package com.project.hrbank.file.handler.impl;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.project.hrbank.file.entity.FileEntity;
 import com.project.hrbank.file.handler.FileHandler;
 
 public class ImageFileHandler implements FileHandler {
 
+	private static final String IMAGE_STORAGE_PATH = "files/images";
+
 	@Override
 	public boolean supports(String extension) {
-		return false;
+		return extension.equalsIgnoreCase("jpg") || extension.equalsIgnoreCase("jpeg") || extension.equalsIgnoreCase("png");
 	}
 
 	@Override
 	public void processFile(MultipartFile file) throws IOException {
-
+		Path filePath = Paths.get(IMAGE_STORAGE_PATH + file.getOriginalFilename());
+		Files.createDirectories(filePath.getParent());
+		Files.write(filePath, file.getBytes());
 	}
 
 	@Override
-	public Resource handleDownload(Resource fileResource, String fileName) throws IOException {
-		return null;
+	public FileEntity handleDownload(Path filePath, String fileName) throws IOException {
+		if(!Files.exists(filePath) || !Files.isReadable(filePath)) {
+			throw new IOException("파일을 읽을 수 없습니다 : " + fileName);
+		}
+		return new FileEntity(null, fileName, "image/jpeg", Files.size(filePath), filePath.toString());
 	}
 }
