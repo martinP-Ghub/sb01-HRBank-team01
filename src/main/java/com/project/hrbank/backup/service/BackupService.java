51f601d2f09c1c272b7f3a20f6e795bcdd94d902
백재우 <9712jw@gmail.com>
2025-03-19 05:12:56 +0900

    Refactor: Backup Service 직원정보 데이터 csv 파일로 저장
    책임분리가 필요하여 이후 리팩토링하겠습니다.

--- src/main/java/com/project/hrbank/backup/service/BackupService.java
+++ src/main/java/com/project/hrbank/backup/service/BackupService.java
@@ -1,149 +1,180 @@
 package com.project.hrbank.backup.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.project.hrbank.backup.domain.Backup;
 import com.project.hrbank.backup.domain.Status;
 import com.project.hrbank.backup.dto.response.BackupDto;
 import com.project.hrbank.backup.dto.response.CursorPageResponseBackupDto;
 import com.project.hrbank.backup.repository.BackupRepository;
 import com.project.hrbank.backup.repository.BackupRepositoryImpl;
+import com.project.hrbank.entity.Employee;
 import com.project.hrbank.file.entity.FileEntity;
+import com.project.hrbank.file.repository.FileRepository;
+import com.project.hrbank.file.storage.FileStorage;
 import com.project.hrbank.repository.EmployeeLogRepository;
-
+import com.project.hrbank.repository.EmployeeRepository;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class BackupService {
-	private static final LocalDateTime POSTGRESQL_MIN_TIMESTAMP = LocalDateTime.of(4713, 11, 24, 0, 0);
-	public static final String SYSTEM_NAME = "SYSTEM";
+    private static final LocalDateTime POSTGRESQL_MIN_TIMESTAMP = LocalDateTime.of(4713, 11, 24, 0, 0);
+    public static final String SYSTEM_NAME = "SYSTEM";
 
-	private final BackupRepository backupRepository;
-	private final EmployeeLogRepository employeeLogRepository;
-	private final BackupRepositoryImpl backupRepositoryImpl;
+    private final BackupRepository backupRepository;
+    private final EmployeeLogRepository employeeLogRepository;
+    private final BackupRepositoryImpl backupRepositoryImpl;
+    private final FileStorage fileStorage;
+    private final EmployeeRepository employeeRepository;
+    private final FileRepository fileRepository;
 
-	public CursorPageResponseBackupDto findAll(LocalDateTime cursor, Pageable pageable) {
-		cursor = Optional.ofNullable(cursor).orElse(LocalDateTime.now());
-		Slice<Backup> slice = backupRepository.findAllBy(cursor, pageable);
+    public CursorPageResponseBackupDto findAll(LocalDateTime cursor, Pageable pageable) {
+        cursor = Optional.ofNullable(cursor).orElse(LocalDateTime.now());
+        Slice<Backup> slice = backupRepository.findAllBy(cursor, pageable);
 
-		List<BackupDto> content = getBackupContents(slice);
+        List<BackupDto> content = getBackupContents(slice);
 
-		LocalDateTime nextCursor = null;
-		if (!slice.getContent().isEmpty()) {
-			nextCursor = slice.getContent().get(slice.getContent().size() - 1).getCreatedAt();
-		}
+        LocalDateTime nextCursor = null;
+        if (!slice.getContent().isEmpty()) {
+            nextCursor = slice.getContent().get(slice.getContent().size() - 1).getCreatedAt();
+        }
 
-		Long nextIdAfter = null;
-		if (slice.hasNext()) {
-			nextIdAfter = content.get(content.size() - 1).id();
-		}
+        Long nextIdAfter = null;
+        if (slice.hasNext()) {
+            nextIdAfter = content.get(content.size() - 1).id();
+        }
 
-		long count = backupRepository.count();
-		return new CursorPageResponseBackupDto(content, nextCursor, nextIdAfter, content.size(), slice.hasNext(), count);
-	}
+        long count = backupRepository.count();
+        return new CursorPageResponseBackupDto(content, nextCursor, nextIdAfter, content.size(), slice.hasNext(),
+                count);
+    }
 
-	private List<BackupDto> getBackupContents(Slice<Backup> slice) {
-		return slice.getContent()
-			.stream()
-			.map(this::toDto)
-			.toList();
-	}
+    private List<BackupDto> getBackupContents(Slice<Backup> slice) {
+        return slice.getContent()
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
 
-	public void backupBySystem() {
-		backup(SYSTEM_NAME);
-	}
+    public void backupBySystem() {
+        backup(SYSTEM_NAME);
+    }
 
-	@Transactional
-	public BackupDto backup(String clientIpAddr) {
-		LocalDateTime lastEndedAtBackupDateTime = getLastEndedAt();
+    @Transactional
+    public BackupDto backup(String clientIpAddr) {
+        LocalDateTime lastEndedAtBackupDateTime = getLastEndedAt();
 
-		Backup backup = Backup.ofInProgress(clientIpAddr);
-		backupRepository.save(backup);
+        Backup backup = Backup.ofInProgress(clientIpAddr);
+        backupRepository.save(backup);
 
-		if (isNotChangedEmployeeInfo(lastEndedAtBackupDateTime)) {
-			backup.updateSkipped();
-			return toDto(backupRepository.save(backup));
-		}
+        if (isNotChangedEmployeeInfo(lastEndedAtBackupDateTime)) {
+            backup.updateSkipped();
+            return toDto(backupRepository.save(backup));
+        }
 
-		// 4 백업 작업을 수행한다.
-		// 4-1 CVS 파일 만들기 -> 파일을 만드는 책임을 분리하자. UserInfoCsvProvider -> createFile() ->
-		// 4-2 생성된 파일에 유저 데이터 적재하기
-		// 4-3 생성된 파일을 멀티파트로 변환하기 -> byte[] 를 Multipart 로 변환해주는 책임을 나누자.
-		// 4-4 파일 데이터베이스에 저장하기 -> FileService 에 변환된 Multipart 파일을 넘겨준다.
-		if (isBackupFail()) {
-			backup.updateFailed();
-			return toDto(backup);
-		}
+        try {
+            FileEntity generateBackupFile = generateBackupFile();
+            backup.updateCompleted(generateBackupFile);
+        } catch (RuntimeException exception) {
+            backup.updateFailed();
+            // 로그 파일에 데이터 쓰기
+        }
+        return toDto(backup);
+    }
 
-		backup.updateCompleted(null);
-		// file repository 를 이용해서 저장된 파일의 메타데이터를 넘겨서 저장한다.
-		return toDto(backup);
-	}
+    private FileEntity generateBackupFile() {
+        String fileName = generateFileName();
+        byte[] employeeData = loadEmployeeData();
+        String contentType = ".csv";
+        return fileStorage.saveFile(null, employeeData, fileName, contentType);
+    }
 
-	// 임시로 만들어둠. 이후 지워야함
-//	private FileEntity createFileTemp() {
-//		return new FileEntity("test", "csv", 10L, "test");
-//	}
+    private byte[] loadEmployeeData() {
+        PrintWriter printWriter = new PrintWriter(new StringWriter());
+        printWriter.println("ID,직원번호,이름,부서,직급,입사일,상태");
+        List<Employee> employees = employeeRepository.findAll();
+        employees.forEach(employee -> {
+            writeEmployeeInfo(employee, printWriter);
+        });
+        return printWriter.toString().getBytes(StandardCharsets.UTF_8);
+    }
 
-	private LocalDateTime getLastEndedAt() {
-		return backupRepository.findLastBackup().stream()
-			.findFirst()
-			.map(Backup::getEndedAt)
-			.orElse(POSTGRESQL_MIN_TIMESTAMP);
-	}
+    private void writeEmployeeInfo(Employee employee, PrintWriter printWriter) {
+        printWriter.printf("%d, %s, %s, %s, %s, %s, %s\n", employee.getEmployeeId(), employee.getEmployeeNumber(),
+                employee.getName(),
+                employee.getDepartmentId(), employee.getPosition(), employee.getHireDate(), employee.getStatus());
+    }
 
-	private boolean isBackupFail() {
-		return false;
-	}
+    private String generateFileName() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+        String formattedDateTime = now.format(formatter);
+        return "backup_employee_" + formattedDateTime;
+    }
 
-	private boolean isNotChangedEmployeeInfo(LocalDateTime endedAt) {
-		return !employeeLogRepository.existsByChangedAtAfter(endedAt);
-	}
 
-	public BackupDto findLatest() {
-		Backup backup = getLastBackup();
-		return toDto(backup);
-	}
+    private LocalDateTime getLastEndedAt() {
+        return backupRepository.findLastBackup().stream()
+                .findFirst()
+                .map(Backup::getEndedAt)
+                .orElse(POSTGRESQL_MIN_TIMESTAMP);
+    }
 
-	private Backup getLastBackup() {
-		return backupRepository.findLastBackup()
-			.stream()
-			.findFirst()
-			.orElseThrow(() -> new NoSuchElementException("Not found Backup"));
-	}
 
-	private BackupDto toDto(Backup backup) {
-		return BackupDto.toDto(backup);
-	}
+    private boolean isNotChangedEmployeeInfo(LocalDateTime endedAt) {
+        return !employeeLogRepository.existsByChangedAtAfter(endedAt);
+    }
 
-	public CursorPageResponseBackupDto findWithSearchCondition(LocalDateTime cursor, Status status, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable) {
-		cursor = Optional.ofNullable(cursor)
-			.orElse(LocalDateTime.now());
-		Slice<Backup> slice = backupRepositoryImpl.findWithSearchCondition(cursor, status, startDate, endDate, pageable);
+    public BackupDto findLatest() {
+        Backup backup = getLastBackup();
+        return toDto(backup);
+    }
 
-		List<BackupDto> content = getBackupContents(slice);
+    private Backup getLastBackup() {
+        return backupRepository.findLastBackup()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Not found Backup"));
+    }
 
-		LocalDateTime nextCursor = null;
-		if (!slice.getContent().isEmpty()) {
-			nextCursor = slice.getContent().get(slice.getContent().size() - 1).getCreatedAt();
-		}
+    private BackupDto toDto(Backup backup) {
+        return BackupDto.toDto(backup);
+    }
 
-		Long nextIdAfter = null;
-		if (slice.hasNext()) {
-			nextIdAfter = content.get(content.size() - 1).id();
-		}
+    public CursorPageResponseBackupDto findWithSearchCondition(LocalDateTime cursor, Status status,
+                                                               LocalDateTime startDate, LocalDateTime endDate,
+                                                               Pageable pageable) {
+        cursor = Optional.ofNullable(cursor)
+                .orElse(LocalDateTime.now());
+        Slice<Backup> slice = backupRepositoryImpl.findWithSearchCondition(cursor, status, startDate, endDate,
+                pageable);
 
-		long count = backupRepository.count();
-		return new CursorPageResponseBackupDto(content, nextCursor, nextIdAfter, content.size(), slice.hasNext(), count);
-	}
+        List<BackupDto> content = getBackupContents(slice);
+
+        LocalDateTime nextCursor = null;
+        if (!slice.getContent().isEmpty()) {
+            nextCursor = slice.getContent().get(slice.getContent().size() - 1).getCreatedAt();
+        }
+
+        Long nextIdAfter = null;
+        if (slice.hasNext()) {
+            nextIdAfter = content.get(content.size() - 1).id();
+        }
+
+        long count = backupRepository.count();
+        return new CursorPageResponseBackupDto(content, nextCursor, nextIdAfter, content.size(), slice.hasNext(),
+                count);
+    }
 }
