81c779e620162f218ac4563b210c957106bcf58b
martin <inb7902@naver.com>
2025-03-19 19:02:12 +0900

    Feat: 날짜 검색 필터 추가

--- src/main/java/com/project/hrbank/repository/EmployeeLogRepository.java
+++ src/main/java/com/project/hrbank/repository/EmployeeLogRepository.java
@@ -15,16 +15,14 @@ import com.project.hrbank.entity.EmployeeLogs;
 public interface EmployeeLogRepository extends JpaRepository<EmployeeLogs, Long> {
 	boolean existsByChangedAtAfter(LocalDateTime lastBackupEndedAt);
 
-	// @Query("SELECT e FROM EmployeeLogs e WHERE e.changedAt < :cursor")
-	// Slice<EmployeeLogs> findAll(@Param("cursor") LocalDateTime cursor, Pageable pageable);
-
 	@Query("SELECT e FROM EmployeeLogs e " +
-		"WHERE e.changedAt < :cursor " +
-		// "AND e.changedAt BETWEEN :atFrom AND :atTo " +
-		"AND (:employeeNumber IS NULL OR e.employeeNumber LIKE %:employeeNumber%) " +
-		"AND (:memo IS NULL OR e.memo LIKE %:memo%) " +
-		"AND (:ipAddress IS NULL OR e.ipAddress LIKE %:ipAddress%) " +
-		"AND (:type IS NULL OR e.type LIKE %:type%)")
+		"WHERE (COALESCE(:cursor, e.changedAt) = e.changedAt OR e.changedAt < :cursor) " +
+		"AND (COALESCE(:atFrom, e.changedAt) = e.changedAt OR COALESCE(:atTo, e.changedAt) = e.changedAt OR e.changedAt BETWEEN :atFrom AND :atTo) "
+		+
+		"AND (:employeeNumber IS NULL OR e.employeeNumber LIKE CONCAT('%', :employeeNumber, '%')) " +
+		"AND (:memo IS NULL OR e.memo LIKE CONCAT('%', :memo, '%')) " +
+		"AND (:ipAddress IS NULL OR e.ipAddress LIKE CONCAT('%', :ipAddress, '%')) " +
+		"AND (:type IS NULL OR e.type LIKE CONCAT('%', :type, '%'))")
 	Slice<EmployeeLogs> findAll(
 		@Param("cursor") LocalDateTime cursor,
 		@Param("employeeNumber") String employeeNumber,
--- src/main/java/com/project/hrbank/service/basic/BasicEmployeeLogService.java
+++ src/main/java/com/project/hrbank/service/basic/BasicEmployeeLogService.java
@@ -34,24 +34,18 @@ public class BasicEmployeeLogService implements EmployeeLogService {
 			EmployeeLogMapper.INSTANT::toDto,
 			EmployeeLogs::getChangedAt,
 			EmployeeLogs::getLog_id,
-			// repository::findAll
 			(cur, page) -> repository.findAll(cur, employeeNumber, memo, ipAddress, type, atFrom, atTo, page)
 		);
 	}
 
 	@Override
 	public String getLogById(Long id) {
+		EmployeeLogs response = repository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("저장되지 않았거나, 삭제된 아이디입니다." + id));
 
-		try {
-			EmployeeLogs response = repository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("저장되지 않았거나, 삭제된 아이디입니다." + id));
+		String diff = response.getChangedValue();
 
-			String diff = response.getChangedValue();
-
-			return diff;
-		} catch (NullPointerException e) {
-			throw new NullPointerException("ID를 찾을 수 없습니다." + e.getMessage());
-		}
+		return diff;
 	}
 
 	@Override
