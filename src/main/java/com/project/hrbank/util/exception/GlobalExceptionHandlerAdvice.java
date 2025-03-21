90bf731ded2e2f5abedc61289fcfdebc72581716
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-21 11:05:17 +0900

    Feature: GlobalException 예외를 처리기능 구현, 세부적인 예외 메시지 수정 필요

---
+++ src/main/java/com/project/hrbank/util/exception/GlobalExceptionHandlerAdvice.java
@@ -0,0 +1,47 @@
+package com.project.hrbank.util.exception;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.project.hrbank.dto.response.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandlerAdvice {
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
+		int status = HttpStatus.NOT_FOUND.value();
+		ErrorResponse errorResponse = ErrorResponse.of(status, "잘못된 요청입니다.", "~는 필수입니다.");
+
+		return ResponseEntity.badRequest().body(errorResponse);
+	}
+
+	@ExceptionHandler(NoSuchElementException.class)
+	protected ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException exception) {
+		int status = HttpStatus.NOT_FOUND.value();
+		ErrorResponse errorResponse = ErrorResponse.of(status, "잘못된 요청입니다.", "리소스를 찾을 수 업습니다.");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	protected ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException exception) {
+		int status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+		ErrorResponse errorResponse = ErrorResponse.of(status, "잘못된 요청입니다.", "리소스를 찾을 수 업습니다.");
+		return ResponseEntity.internalServerError().body(errorResponse);
+	}
+
+}
+
+/**
+ * {
+ *   "timestamp": "2025-03-06T05:39:06.152068Z",
+ *   "status": 400,
+ *   "message": "잘못된 요청입니다.",
+ *   "details": "부서 코드는 필수입니다."
+ * }
+ */
